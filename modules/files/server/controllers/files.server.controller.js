6145bbe3cd5658ec9e37c2215c2ef9d12f819a26
rohitg15 <rohitg5249@gmail.com>
2016-12-05 19:07:04 -0500

    Updated insert to upload files to multiple s3 buckets. Implemented read to download files from either one of two s3 buckets

--- modules/files/server/controllers/files.server.controller.js
+++ modules/files/server/controllers/files.server.controller.js
@@ -13,6 +13,24 @@ var path = require('path'),
   fs = require('fs'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
+var bucket_names = ["cloudbucketnextgen","cloudbucketnextgenbackup"];
+
+function get_file_from_s3(s3, bucket_names, i, key) {
+  var params = {Bucket : bucket_names[i], Key : key};
+  s3.getObject(params, function(err, data) {
+    if (err) {
+      if (i === bucket_names.length) {
+        return [err, false];
+      }
+      else {
+        get_file_from_s3(bucket_names, i + 1, key);
+      }
+    }
+    else {
+      return [data, true];
+    }
+  }); 
+}
 
 /**
  * Upload a file
@@ -21,7 +39,7 @@ exports.create = function (req, res) {
   var user = req.user;
   var upload = multer(config.uploads.s3FileUpload).single('fileItem');
 
-  var bucket_name = "cloudbucketnextgen";
+  var bucket_name = bucket_names[0];
   var s3 = new aws_client.S3();
   
   if (user) {
@@ -43,34 +61,48 @@ exports.create = function (req, res) {
           } 
           else {
             console.log("data file was stored into memory");
-            var params = {Bucket : bucket_name, Key : file.user._id + "/" + file.filename, Body : data };
             console.log("bucket_name:" + bucket_name);
             console.log("key:" + file.user._id + "/" + file.filename);
             //  console.log("data:" + data);
-            s3.putObject(params, function(err, data) {
-              if (err) {
-                return res.status(400).send({
-                  message: err
-                });
-              } 
-              else {
-                //  object saved in s3 - store metadata in mongodb
-                // save file details 
-                console.log("file uploaded to s3");
-                file.save(function (err) {
-                  if (err) {
-                    return res.status(400).send({
-                      message: errorHandler.getErrorMessage(err)
-                    });
-                  } 
-                  else {
-                    fs.unlink(file.path);
-                    res.json(file);
-                  }
-                });
-                  
-              }
-            });
+          //  for(var i=0; i<bucket_names.length; ++i) {
+              var params = {Bucket : bucket_names[0], Key : file.user._id + "/" + file.filename, Body : data };
+            
+              s3.putObject(params, function(err, data) {
+                if (err) {
+                  return res.status(400).send({
+                    message: err
+                  });
+                } 
+                else {
+                  //  object saved in s3 - store metadata in mongodb
+                  // save file details 
+                  console.log("file uploaded to s3");
+                  file.save(function (err) {
+                    if (err) {
+                      return res.status(400).send({
+                        message: errorHandler.getErrorMessage(err)
+                      });
+                    } 
+                    else {
+                      fs.unlink(file.path);
+                      res.json(file);
+                    }
+                  });
+                }
+              });
+            //}
+
+            for(var i=1; i<bucket_names.length; ++i) {
+              var parameters = {Bucket : bucket_names[i], Key : file.user._id + "/" + file.filename, Body : data };
+              s3.putObject(parameters, function(err, data) {
+                if (err) {
+                  // should we return a 400 here?
+                  return res.status(400).send({
+                    message: err
+                  });
+                }
+              }); 
+            }
           }
         });
       }
@@ -85,6 +117,55 @@ exports.read = function (req, res) {
   
     // data from s3
     // fs.write(file.path)
+  var user = req.user;
+  var file = req.file;
+  var s3 = new aws_client.S3();
+  if (user) {
+    var i=0;
+    var bucket_name = bucket_names[0];
+    var params = {Bucket : bucket_names[0], Key : file.user._id + "/" + file.filename};
+    s3.getObject(params, function(err, data) {
+      if (err) {
+        console.log("coiuld not retrieve file from primary!");
+        var params2 = {Bucket : bucket_names[1], Key : file.user._id + "/" + file.filename};
+        s3.getObject(params2, function(err, data){
+          if (err) {
+            console.log("error in second access!");
+            return res.status(400).send({
+              message: err
+            });
+          }
+          else {
+            console.log("retreived file from secondary!");
+            fs.writeFile(file.path, data, function(err, data) {
+              if (err) {
+                return res.status(400).send({
+                  message: err
+                });
+              }
+              else {
+                console.log("file retrieved from secondary and saved!");
+              }
+            });              
+          }
+        });
+      }
+      else {
+        console.log("file retrieved from primary!" + data);
+        fs.writeFile(req.file.path, data, function(err, data) {
+          if (err) {
+            return res.status(400).send({
+              message: err
+            });
+          }
+          else {
+            console.log("file retrieved from primary and saved!");
+          }
+        });   
+            
+      }
+    });
+  } 
   res.json(req.file);
 };
 
